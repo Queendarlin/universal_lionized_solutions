d4744ba3a8bf44e54ef25cc24c1a04338394059a
Queendarlin <queendarlinnnamani@gmail.com>
2025-01-15 10:17:37 +0100

    Add correct database details

--- setup.sh
+++ setup.sh
@@ -10,4 +10,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py tailwind install
 python manage.py collectstatic --noinput
-python manage.py tailwind start
+python manage.py tailwind build
