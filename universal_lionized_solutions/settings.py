dccdb86008a0425d2ad52041c2a5cc2194d15eea
Queendarlin <queendarlinnnamani@gmail.com>
2025-01-22 10:00:17 +0100

    Debug 404 error for css

--- universal_lionized_solutions/settings.py
+++ universal_lionized_solutions/settings.py
@@ -53,6 +53,7 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 
 
 TAILWIND_APP_NAME = 'theme'
+TAILWIND_CSS_PATH = 'css/dist/styles.css'
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -150,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
